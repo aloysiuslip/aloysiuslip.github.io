f0b76300a22d659323bdc93793d3d39b293404af
theLAZYmd <lazymeep+github@gmail.com>
2019-09-26 18:49:39 +0100

    Updates

---
+++ precache-manifest.ea229d18a001f1402d568fe44932180d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de21328aa72345e6c1d2fd3a23ba122c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc38be9a511b4d5e85a9",
+    "url": "/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "4c9bdf2d4291a1aabe57",
+    "url": "/static/css/main.ebe99235.chunk.css"
+  },
+  {
+    "revision": "dc38be9a511b4d5e85a9",
+    "url": "/static/js/2.db3953b1.chunk.js"
+  },
+  {
+    "revision": "4c9bdf2d4291a1aabe57",
+    "url": "/static/js/main.299aa01d.chunk.js"
+  },
+  {
+    "revision": "09ba5c9d33e1053c0e1d",
+    "url": "/static/js/runtime~main.607aed3d.js"
+  },
+  {
+    "revision": "b42012dbc635cdd4f170ece2084e474f",
+    "url": "/static/media/bot.b42012db.png"
+  },
+  {
+    "revision": "220203d9a16820ebba40d42bdc5dc30f",
+    "url": "/static/media/djs.220203d9.png"
+  },
+  {
+    "revision": "19f6f5e74c95428be9ac5067e3b86a8d",
+    "url": "/static/media/ecf.19f6f5e7.png"
+  },
+  {
+    "revision": "2c121c2e05a992535f6c8f1204c52a24",
+    "url": "/static/media/facebook.2c121c2e.png"
+  },
+  {
+    "revision": "c298f843fecda9871fd82cf6a7a0c456",
+    "url": "/static/media/github.c298f843.png"
+  },
+  {
+    "revision": "91540e799a1cf30a43a371b813deea25",
+    "url": "/static/media/hackmit.91540e79.png"
+  },
+  {
+    "revision": "2039dab42c532a502f5599badee9ebc3",
+    "url": "/static/media/instagram.2039dab4.png"
+  },
+  {
+    "revision": "4c42629f687127cbcba618cf0be150ba",
+    "url": "/static/media/lichess.4c42629f.png"
+  },
+  {
+    "revision": "b424c19c2e911cd666a6ecbf522647e6",
+    "url": "/static/media/linkedin.b424c19c.png"
+  },
+  {
+    "revision": "925b18ffe331a412ea8b5d53f90b77e2",
+    "url": "/static/media/npm.925b18ff.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/static/media/reactjs.47ce6e77.png"
+  },
+  {
+    "revision": "f43644323e5000f17c7ecb76291a96ed",
+    "url": "/static/media/send.f4364432.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aloysius-lip/precache-manifest.08f72bf53058ac25d3fe1337aefe8b58.js"
+  "/precache-manifest.ea229d18a001f1402d568fe44932180d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aloysius-lip/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
