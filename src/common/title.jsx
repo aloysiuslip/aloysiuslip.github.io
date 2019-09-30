c338e0779bc92b54ca4c52d3580e736641a30356
theLAZYmd <theLAZYmd.01@gmail.com>
2019-09-30 17:11:38 +0100

    Title to rel router path not a

--- src/common/title.jsx
+++ src/common/title.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default class Title extends React.Component {
 
 	render() {
 		return (
 			<div className="title">
-				<a href="/">
+				<Link to="/">
 					<h1>Aloysius Lip</h1>
-				</a>
+				</Link>
 			</div>
 		);
 	}
